dc1efeb0b02997fa5dd2bb54099fe51affb13854
HMZHZDH7 <56533642+HMZHZDH7@users.noreply.github.com>
2024-04-05 03:42:41 +0200

    Cannon version

--- actions/actions.py
+++ actions/actions.py
@@ -17,9 +17,9 @@ from rasa_sdk.types import DomainDict
 # SOCKET = sockets.Socket()
 PLOT_HANDLER = plot_handler.PlotHandler()
 ALLOWED_PLOT_TYPES = ["line", "bar", "pie", "barh"]
-ALLOWED_SELECTED_VALUES = ["age", "gender", "hospital_stroke", "hospitalized_in", "department_type", "stroke_type",
+ALLOWED_SELECTED_VALUES = ["age", "gender", "hospital_stroke", "stroke_type",
                            "nihss_score", "thrombolysis", "no_thrombolysis_reason", "door_to_needle", "door_to_imaging",
-                           "onset_to_door", "imaging_done", "imaging_type", "dysphagia_screening_type",
+                           "onset_to_door", "imaging_done", "imaging_type", "dysphagia_screening_type", "door_to_groin"
                            "before_onset_antidiabetics", "before_onset_cilostazol", "before_onset_clopidrogel",
                            "before_onset_ticagrelor", "before_onset_ticlopidine", "before_onset_prasugrel",
                            "before_onset_dipyridamol", "before_onset_warfarin", "risk_hypertension", "risk_diabetes",
@@ -48,14 +48,13 @@ class ActionChangePlottype(Action):
 
         if plot_type:
             if plot_type.lower() not in ALLOWED_PLOT_TYPES:
-                dispatcher.utter_message(text=f"Sorry, I can only create {'/'.join(ALLOWED_PLOT_TYPES)} plots.")
+                dispatcher.utter_message(text=f"Sorry, I don't understand. Can you try rephrasing?")
                 return {"plot_type": None}
-            dispatcher.utter_message(text=f"OK! I will create a {plot_type} plot.")
+            #dispatcher.utter_message(text=f"OK! I will create a {plot_type} plot.")
 
         PLOT_HANDLER.change_arg("type", plot_type)
 
-        response = PLOT_HANDLER.send_args()
-        dispatcher.utter_message(text=f"{response}")
+        PLOT_HANDLER.send_args()
 
         return []
 
@@ -72,14 +71,13 @@ class ActionChangeSelectedvalue(Action):
 
         if selected_value:
             if selected_value.lower() not in ALLOWED_SELECTED_VALUES:
-                dispatcher.utter_message(text=f"Sorry, I can only create {'/'.join(ALLOWED_SELECTED_VALUES)} plots.")
+                dispatcher.utter_message(text=f"Sorry, I don't understand. Can you try rephrasing?")
                 return {"selected_value": None}
             dispatcher.utter_message(text=f"OK! I will create a {selected_value} plot.")
 
         PLOT_HANDLER.change_arg("variable", selected_value)
 
-        response = PLOT_HANDLER.edit_data(tracker.get_slot("nat_value"))
-        dispatcher.utter_message(text=f"{response}")
+        PLOT_HANDLER.edit_data(tracker.get_slot("nat_value"))
 
         return []
 
@@ -98,9 +96,7 @@ class ActionToggleNationalValue(Action):
 
         PLOT_HANDLER.change_arg("show_nat_val", new_value)
         PLOT_HANDLER.send_args()
-        response = PLOT_HANDLER.edit_data(new_value)
-
-        dispatcher.utter_message(text=f"{response}")
+        PLOT_HANDLER.edit_data(new_value)
 
         dispatcher.utter_message(text="We are {} the national value.".format("showing" if new_value else "hiding"))
 
@@ -118,10 +114,13 @@ class PrefillSlots(Action):
 
         selected_value = "door_to_needle"
 
+        real_diff = False
+
         return [
             SlotSet("plot_type", plot_type),
             SlotSet("nat_value", nat_value),
-            SlotSet("selected_value", selected_value)
+            SlotSet("selected_value", selected_value),
+            SlotSet("real_diff", real_diff)
         ]
 
 
@@ -136,8 +135,9 @@ class ActionInitialise(Action):
 
         PLOT_HANDLER.change_arg("type", plot_type)
         PLOT_HANDLER.change_arg("show_nat_val", nat_value)
+        PLOT_HANDLER.change_arg("variable", selected_value)
         PLOT_HANDLER.send_args()
-        PLOT_HANDLER.edit_data(selected_value)
+        PLOT_HANDLER.edit_data(nat_value)
 
         return []
 
@@ -151,24 +151,33 @@ class ActionVariableTTest(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         # Access dataframe from the tracker
-        data = tracker.get_slot("data")
+        #data = tracker.get_slot("data")
 
         # Get the variable name from the tracker
-        variable_name = tracker.get_slot("variable_name")
+        #variable_name = tracker.get_slot("variable_name")
 
         # Call the function to perform t-test
         p_value, cohens_d, no_2022_q2_data = PLOT_HANDLER.compare_to_past()
 
         # Construct message based on results
         if no_2022_q2_data:
-            message = f"There was no data available for 2022 Q2. Comparing 2022 Q1 to 2021 Q2, the p-value of the wilcoxon test is {p_value:.4f}, Cohen's d is {cohens_d:.4f}."
+            message = f"There was no data available for 2022 Q2. Comparing 2022 Q1 to 2021 Q4, the p-value of the wilcoxon test is {p_value:.4f}, Cohen's d is {cohens_d:.4f}."
         else:
             message = f"Comparing 2022 Q2 to 2022 Q1, the p-value of the wilcoxon test is {p_value:.4f}, Cohen's d is {cohens_d:.4f}."
 
         # Utter the message
         dispatcher.utter_message(text=message)
 
-        return []
+        if p_value < 0.1:
+            dispatcher.utter_message(text=f"The p-value tells us that there is a {p_value * 100} difference between quarters, which means the difference is likely real.")
+            dispatcher.utter_message("Shall I use my A.I. abilities to find some possible causes for this difference? Or would you rather analyse the impact of this change on patient outcomes?")
+            real_diff = True
+        else:
+            dispatcher.utter_message(text=f"The p-value tells us that there is a {p_value * 100} difference between quarters, which means the difference is likely cause by random chance and variance.")
+            dispatcher.utter_message("What variable would you like to explore next?")
+            real_diff = False
+
+        return [SlotSet("real_diff", real_diff)]
 
 
 class ActionFindPredictors(Action):
@@ -191,13 +200,19 @@ class ActionFindPredictors(Action):
                                        feature_weights['Shap Values'].items()}
 
             # Format the feature weights as a response
-            dispatcher.utter_message(f"Root Mean Squared Error: {mean_error}")
-            dispatcher.utter_message("\n\nFeature Importances:\n")
+            ##dispatcher.utter_message(f"Root Mean Squared Error: {mean_error}")
+            ##dispatcher.utter_message("\n\nFeature Importances:\n")
 
             selected_value = tracker.get_slot("selected_value")
+            dispatcher.utter_message(f"I built a seperate model to predict {selected_value} based only on data from your hospital")
+            dispatcher.utter_message(f"The most important factors are:")
             # Send feature importances as separate messages
-            for feature, weight in rounded_feature_weights.items():
-                dispatcher.utter_message(f"On average, {feature} increases your {selected_value} by: {weight}")
+            features_str = ", ".join(rounded_feature_weights.keys())
+
+            # Send a single message with all features separated by commas
+            dispatcher.utter_message(features_str)
+
+            dispatcher.utter_message(f"Would you rather: explore how {selected_value} effects you patients or do you want to explore one of the variables I have listed?")
 
         return []
 
@@ -205,7 +220,7 @@ class ActionFindPredictors(Action):
 class ActionFindPredictorsGlobal(Action):
 
     def name(self) -> Text:
-        return "action_find_predictors_global"
+        return "action_global_predictions"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
@@ -222,13 +237,72 @@ class ActionFindPredictorsGlobal(Action):
                                        feature_weights['Shap Values'].items()}
 
             # Format the feature weights as a response
-            dispatcher.utter_message(f"Root Mean Squared Error: {mean_error}")
-            dispatcher.utter_message("\n\nFeature Importances:\n")
-
+            #dispatcher.utter_message(f"Root Mean Squared Error: {mean_error}")
+            #dispatcher.utter_message("\n\nFeature Importances:\n")
             selected_value = tracker.get_slot("selected_value")
+            dispatcher.utter_message(f"I used the data I have from multiple hospitals including your to buld a regression model that predicts {selected_value}.")
+            dispatcher.utter_message(f"Based on my model, here are the indicators which influence {selected_value} the most, in order or largest to smallest influence:")
             # Send feature importances as separate messages
-            for feature, weight in rounded_feature_weights.items():
-                dispatcher.utter_message(f"On average, {feature} increases your {selected_value} by: {weight}")
+            # Concatenate features into a single string with commas
+            features_str = ", ".join(rounded_feature_weights.keys())
+
+            # Send a single message with all features separated by commas
+            dispatcher.utter_message(features_str)
 
         return []
 
+
+class ActionExploreEffects(Action):
+
+    def name(self) -> Text:
+        return "action_explore_effects"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        selected_value = 'discharge_mrs'
+        PLOT_HANDLER.change_arg('type', selected_value)
+        PLOT_HANDLER.edit_data(False)
+        PLOT_HANDLER.send_args()
+
+        return [SlotSet("selected_value", selected_value)]
+
+
+class ActionDNTGuideline(Action):
+    def name(self) -> Text:
+        return "action_dnt_guideline"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        response = PLOT_HANDLER.DNT_guideline()
+        dispatcher.utter_message("The first guideline is that 75% of patients must be treated within 45 minutes..")
+        dispatcher.utter_message("It looks like your hospital does meet the guideline for DNT. Should I continue checking more guidelines?")
+        dispatcher.utter_message("The next one is about screening at least 90% of patients for dysphagia.")
+
+        return []
+
+class ActionDysphagiaGuideline(Action):
+    def name(self) -> Text:
+        return "action_dysphagia_guideline"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        response = PLOT_HANDLER.dysphagia_guideline()
+
+        dispatcher.utter_message("While your hospital does screen many patients, you still do not meet the guideline on average. However, you did meet it for the most current quarter.")
+        dispatcher.utter_message("Should we check the last guideline? It's about 90% of elligible patients getting anticoagulents.")
+
+
+        return []
+
+class ActionAnticoagsGuideline(Action):
+    def name(self) -> Text:
+        return "action_anticoag_guideline"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        response = PLOT_HANDLER.guideline_anticoags()
+
+        dispatcher.utter_message("Again, your hospital provides the correct medication to many patients but does not meet the guideline of 90% except for the most redent quarter..")
+        dispatcher.utter_message("This is the last of the guidelines, let me know if you would like to explore somehting else.")
+
+
+        return []
--- actions/utils/plot_handler.py
+++ actions/utils/plot_handler.py
@@ -17,8 +17,8 @@ class PlotHandler:
         self._save = save_plot
         self.json_file_path = "actions/utils/plot_args.json"
         self.json_data_path = "actions/utils/data.json"
-        #self.website_url = "http://localhost:3000/rasa-webhook"
-        self.website_url = "https://dashboards.create.aau.dk/rasa-webhook"
+        self.website_url = "http://localhost:3000/rasa-webhook"
+        #self.website_url = "https://dashboards.create.aau.dk/rasa-webhook"
         self.data = pd.read_csv("actions/utils/dataREanonymized_long.csv")
 
     def change_arg(self, arg, value):
@@ -126,11 +126,6 @@ class PlotHandler:
         mean_diff = q2_data.mean() - q1_data.mean()
         pooled_std = np.sqrt((q1_data.var() + q2_data.var()) / 2)
         cohens_d = mean_diff / pooled_std
-
-        print("Group 2 mean: ", q2_data.mean())
-        print("Group 1 mean: ", q1_data.mean())
-        print("Group 2 std: ", q2_data.std())
-        print("Group 1 std: ", q1_data.std())
 
 
         # Flag to indicate if we did not have data from 2022 Q2
@@ -243,3 +238,222 @@ class PlotHandler:
 
         # Return error (if any) and feature weights
         return None, {'Root Mean Squared Error': accuracy, 'Feature Importances': feature_weights, 'Shap Values': shap_values}
+
+    def DNT_guideline(self):
+        with open(self.json_file_path, 'r') as json_file:
+            config = json.load(json_file)
+
+        data = self.data
+        show_nat = config['visualization']['show_nat_val']
+
+        df_filtered = data[data['site_id'].isin(["Vitality"])]
+
+        # Filter the DataFrame to keep only 'door_to_needle' variable
+        filtered_df = df_filtered[df_filtered['variable'] == 'door_to_needle'].copy()
+
+        filtered_df['Value'] = pd.to_numeric(filtered_df['Value'], errors='coerce')
+        filtered_df = filtered_df.dropna(subset=['Value'])
+
+        # Calculate the percentage of patients with door to needle time below 60 minutes in each quarter
+        filtered_df['door_to_needle_below_60'] = filtered_df['Value'] < 45
+        percentage_below_60 = filtered_df.groupby('YQ')['door_to_needle_below_60'].mean() * 100
+
+        percentage_below_60_df = percentage_below_60.reset_index()
+        percentage_below_60_df.columns = ['YQ', 'Value']
+
+        if show_nat == True:
+            df_filtered_nat = data[~data['site_id'].isin(["Vitality"])]
+            filtered_df_nat = df_filtered_nat[df_filtered_nat['variable'] == 'door_to_needle'].copy()
+
+            filtered_df_nat['Value'] = pd.to_numeric(filtered_df_nat['Value'], errors='coerce')
+            filtered_df_nat = filtered_df_nat.dropna(subset=['Value'])
+
+            # Calculate the percentage of patients with door to needle time below 60 minutes in each quarter
+            filtered_df_nat['door_to_needle_below_60'] = filtered_df_nat['Value'] < 45
+            percentage_below_60_nat = filtered_df_nat.groupby('YQ')['door_to_needle_below_60'].mean() * 100
+
+            percentage_below_60_df_nat = percentage_below_60_nat.reset_index()
+            percentage_below_60_df_nat.columns = ['YQ', 'Value']
+            percentage_below_60_df['nat_value'] = percentage_below_60_df_nat['Value']
+            # print(percentage_below_60_nat
+
+        json_data = percentage_below_60_df.to_json(orient='records')
+
+        with open(self.json_data_path, 'w') as json_file:
+            json_file.write(json_data)
+
+        with open(self.json_data_path, 'r') as json_file:
+            config = json.load(json_file)
+
+        compressed_content = gzip.compress(json.dumps(config).encode("utf-8"))
+        compressed_content_decoded = base64.b64encode(compressed_content).decode("utf-8")
+
+        payload = {"file_type": "data", "file_content": compressed_content_decoded}
+
+        response = requests.post(self.website_url, json=payload)
+
+        print(response.status_code)
+
+        return response.json()
+
+    def dysphagia_guideline(self):
+        with open(self.json_file_path, 'r') as json_file:
+            config = json.load(json_file)
+
+        data = self.data
+        show_nat = config['visualization']['show_nat_val']
+
+        df_filtered = data[data['site_id'].isin(["Vitality"])]
+        # Filter the DataFrame to keep only 'dysphagia_screening_type' variable
+        filtered_df = df_filtered[df_filtered['variable'] == 'dysphagia_screening_type'].copy()
+
+        # Count non-null values for each quarter
+        non_null_counts = filtered_df.groupby('YQ')['Value'].count()
+
+        # Count total number of patients for each quarter
+        total_patients = df_filtered.groupby('YQ')['subject_id'].nunique()
+
+        # Calculate percentage of patients with non-null values for each quarter
+        percentage_filled_in = (non_null_counts / total_patients) * 100
+
+        # Convert percentage_filled_in Series to a DataFrame
+        percentage_filled_in_df = percentage_filled_in.reset_index()
+        percentage_filled_in_df.columns = ['YQ', 'Value']
+
+        if show_nat == True:
+            df_filtered_nat = data[~data['site_id'].isin(["Vitality"])]
+            # Filter the DataFrame to keep only 'dysphagia_screening_type' variable
+            filtered_df_nat = df_filtered_nat[df_filtered_nat['variable'] == 'dysphagia_screening_type'].copy()
+
+            # Count non-null values for each quarter
+            non_null_counts_nat = filtered_df_nat.groupby('YQ')['Value'].count()
+
+            # Count total number of patients for each quarter
+            total_patients_nat = df_filtered_nat.groupby('YQ')['subject_id'].nunique()
+
+            # Calculate percentage of patients with non-null values for each quarter
+            percentage_filled_in_nat = (non_null_counts_nat / total_patients_nat) * 100
+
+            # Convert percentage_filled_in Series to a DataFrame
+            percentage_filled_in_df_nat = percentage_filled_in_nat.reset_index()
+            percentage_filled_in_df_nat.columns = ['YQ', 'Value']
+            percentage_filled_in_df['nat_value'] = percentage_filled_in_df_nat['Value']
+
+        json_data = percentage_filled_in_df.to_json(orient='records')
+
+        with open(self.json_data_path, 'w') as json_file:
+            json_file.write(json_data)
+
+        with open(self.json_data_path, 'r') as json_file:
+            config = json.load(json_file)
+
+        compressed_content = gzip.compress(json.dumps(config).encode("utf-8"))
+        compressed_content_decoded = base64.b64encode(compressed_content).decode("utf-8")
+
+        payload = {"file_type": "data", "file_content": compressed_content_decoded}
+
+        response = requests.post(self.website_url, json=payload)
+
+        print(response.status_code)
+
+        return response.json()
+
+    def guideline_anticoags(self):
+        with open(self.json_file_path, 'r') as json_file:
+            config = json.load(json_file)
+
+        data = self.data
+        show_nat = config['visualization']['show_nat_val']
+
+        df_filtered = data[data['site_id'].isin(["Vitality"])]
+
+        # Filter the DataFrame to keep only the variables of interest
+        filtered_df = df_filtered[
+            df_filtered['variable'].isin(['discharge_warfarin', 'discharge_heparin', 'discharge_dabigatran',
+                                          'discharge_rivaroxaban', 'discharge_apixaban', 'discharge_edoxaban',
+                                          'discharge_cilostazol',
+                                          'discharge_clopidrogel',
+                                          'discharge_ticagrelor',
+                                          'discharge_ticlopidine',
+                                          'discharge_prasugrel',
+                                          'discharge_dipyridamol',
+                                          ])].copy()
+
+        # Convert 'Value' column to numeric
+        filtered_df['Value'] = pd.to_numeric(filtered_df['Value'], errors='coerce')
+
+        # Replace NaN values with 0
+        filtered_df['Value'].fillna(0, inplace=True)
+
+        # Mark patients with 1 in any of the variables
+        filtered_df['any_discharge_medication'] = (filtered_df['Value'] == 1).astype(int)
+
+        # Count patients with 1 in any discharge medication for each quarter
+        count_any_discharge_medication = filtered_df.groupby('YQ')['any_discharge_medication'].sum()
+
+        # Count total number of patients for each quarter
+        total_patients = df_filtered.groupby('YQ')['subject_id'].nunique()
+
+        # Calculate percentage of patients with 1 in any discharge medication for each quarter
+        percentage_any_discharge_medication = (count_any_discharge_medication / total_patients) * 100
+
+        # Convert percentage_any_discharge_medication Series to a DataFrame
+        percentage_any_discharge_medication_df = percentage_any_discharge_medication.reset_index()
+        percentage_any_discharge_medication_df.columns = ['YQ', 'Value']
+
+        if show_nat == True:
+            df_filtered_nat = data[~data['site_id'].isin(["Vitality"])]
+
+            # Filter the DataFrame to keep only the variables of interest
+            filtered_df_nat = df_filtered_nat[
+                df_filtered_nat['variable'].isin(['discharge_warfarin', 'discharge_heparin', 'discharge_dabigatran',
+                                                  'discharge_rivaroxaban', 'discharge_apixaban', 'discharge_edoxaban',
+                                                  'discharge_cilostazol',
+                                                  'discharge_clopidrogel',
+                                                  'discharge_ticagrelor',
+                                                  'discharge_ticlopidine',
+                                                  'discharge_prasugrel',
+                                                  'discharge_dipyridamol',
+                                                  ])].copy()
+
+            # Convert 'Value' column to numeric
+            filtered_df_nat['Value'] = pd.to_numeric(filtered_df_nat['Value'], errors='coerce')
+
+            # Replace NaN values with 0
+            filtered_df_nat['Value'].fillna(0, inplace=True)
+
+            # Mark patients with 1 in any of the variables
+            filtered_df_nat['any_discharge_medication'] = (filtered_df_nat['Value'] == 1).astype(int)
+
+            # Count patients with 1 in any discharge medication for each quarter
+            count_any_discharge_medication_nat = filtered_df_nat.groupby('YQ')['any_discharge_medication'].sum()
+
+            # Count total number of patients for each quarter
+            total_patients_nat = df_filtered_nat.groupby('YQ')['subject_id'].nunique()
+
+            # Calculate percentage of patients with 1 in any discharge medication for each quarter
+            percentage_any_discharge_medication_nat = (count_any_discharge_medication_nat / total_patients_nat) * 100
+
+            # Convert percentage_any_discharge_medication Series to a DataFrame
+            percentage_any_discharge_medication_df_nat = percentage_any_discharge_medication_nat.reset_index()
+            percentage_any_discharge_medication_df_nat.columns = ['YQ', 'Value']
+            percentage_any_discharge_medication_df['nat_value'] = percentage_any_discharge_medication_df_nat['Value']
+
+            json_data = percentage_any_discharge_medication_df.to_json(orient='records')
+
+            with open(self.json_data_path, 'w') as json_file:
+                json_file.write(json_data)
+
+            with open(self.json_data_path, 'r') as json_file:
+                config = json.load(json_file)
+
+            compressed_content = gzip.compress(json.dumps(config).encode("utf-8"))
+            compressed_content_decoded = base64.b64encode(compressed_content).decode("utf-8")
+
+            payload = {"file_type": "data", "file_content": compressed_content_decoded}
+
+            response = requests.post(self.website_url, json=payload)
+
+            print(response.status_code)
+
+            return response.json()
